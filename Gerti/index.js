1482d9adeb59897f2e4598dada2adaa16efa4277
Gerti Islami <gertiislami34@gmail.com>
2025-11-20 20:01:19 +0100

    collectiong student(gerti)

--- Gerti/index.js
+++ Gerti/index.js
@@ -1,141 +1,15 @@
-//1️⃣ Funksion për rolin e përdoruesit
 
-//Detyra:
-//Krijo funksionin getUserAccess(user) që pranon një objekt me name dhe role.
-//Funksioni duhet të kthejë një tekst të ndryshëm varësisht rolit të përdoruesit.
+    let classroom = [
+        { name: "Arta", grade: 9 },
+        { name: "Luan", grade: 8 },
+        { name: "Elena", grade: 10 }
+        ];
 
-//Hint:
-//Përdor if/else. Shembull:
-//Nëse user.role === "admin" → kthe “Full access”.
-
-
-function getUserAccess(user)
-{
-    if (user.role === "admin")
-    {
-        return "Full access";
-    }
-}
-
-const adminUser ={name:"Gerti" role:"admin"}
-const adminUser ={name:"Islami" role:"editor"}
-
-
-//2️⃣ Moti në qytet
-
-//Detyra:
-//Krijo funksionin checkWeather(weather) që merr objekt me temperature dhe city, dhe kthen përshkrimin e motit.
-
-//Rregullat:
-//• 30 → nxehtë
-//• 20–30 → ngrohtë
-//• <20 → ftohtë
-
-//Hint:
-//Kontrollo temperaturën duke përdorur if, else if, else.
-
-function checkWeather(weather)
-{
-    if (weather.temperature > 30)
-    {
-        return 'nxehte'
-    }
-    else if ( weather.temperature >= 20 && weather.temperature <= 30)
-    {
-        return 'ngrohte'
-    }
-    else
-    {
-        return 'ftohte'
-    }
-}
-
-
-
-/*
-3️⃣ Llogaritja e çmimit me switch
-
-Detyra:
-Krijo funksionin getPrice(productName) që përdor switch për të kthyer çmimin e produkteve si: milk, bread, eggs.
-
-Hint:
-Përdor strukturën switch (productName) { case "milk": ... }.*/
-
-function getPrice(productName)
-{
-    switch (productName)
-    {
-        case "milk":
-            return 1;
-        case "bread":
-            return 2;
-        case "eggs":
-            return 3; 
-        default:
-            return "not found";
-    }   
-}
-
-
-/*
-4️⃣ Notat e studentëve
-
-Detyra:
-Krijo funksionin checkGrade(student) që merr një objekt me name dhe grade, dhe kthen vlerësimin sipas pikëve.
-
-Hint:
-Përdor disa else if për të ndarë notat:
-(shkëlqyeshëm, shumë mirë, mirë, mjaftueshëm, dobët).*/
-
-function checkGrade(student)
-{
-    if ( student.grade >= 90)
-    {
-        return "shkelqyeshem";
-    }
-    else if ( student.grade >= 80)
-    {
-        return "shumë mir";
-    }
-    else if (student.grade >= 70)
-    {
-        return "mire"
-    }
-    else if (student.grade >=60)
-    {
-        return "mjaftueshem"
-    }
-    else if (student.grade >= 50)
-    {    
-        return "dobet"
-    }    
-}
-
-/*5️⃣ Kontrolli i makinës
-
-Detyra:
-Krijo funksionin checkCar(car) që merr objekt me brand dhe fuel.
-Nëse makina është elektrike → printo “Eco friendly car”, ndryshe “Uses fuel”.
-
-Hint:
-Krahaso: car.fuel === "electric".*/
-
-
-function checkCar (car)
-
-{
-    if (car.fuel === "electric")
-    {
-        return "eco friendly car "
-    }
-    else 
-    {
-        return "user fuel"
-    }
-}
-
-const myCar = {brand: "Tesla", fuel: "electric"};
-const myCar2 = {brand:"bmw", fuel: "diesel"};
-
-console.log(checkCar(myCar));
-console.log(checkCar(myCar2));
+        let result = 0;
+        
+        for (let i = 0; i < classroom.length; i++){
+            result = result + classroom[i].grade;
+        }
+        console.log(result)
+     
+    
